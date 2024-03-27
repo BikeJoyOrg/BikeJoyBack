ff41d052b6ca97a9bb7fc60e2de5bfd710fc480d
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-03-27 21:29:39 +0100

    modificacions setting

--- Backend/apiCrud/settings.py
+++ Backend/apiCrud/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['nattech.fib.upc.edu',"127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = ['HTTPS://nattech.fib.upc.edu']
+CSRF_TRUSTED_ORIGINS = ['https://nattech.fib.upc.edu']
 # Application definition
 
 INSTALLED_APPS = [
