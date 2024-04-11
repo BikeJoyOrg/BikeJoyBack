e4e912d8d22b4a9e6f91de2faf5e44a437243ec2
nil.del.pozo <nil.del.pozo@estudiantat.upc.edu>
2024-04-11 19:04:41 +0200

    app items sense fotos

--- Backend/Items/serializers.py
+++ Backend/Items/serializers.py
@@ -7,15 +7,7 @@ from .models import Item
 
 
 class ItemSerializer(serializers.ModelSerializer):
-    item_picture = serializers.SerializerMethodField('get_image')
-
     class Meta:
         model = Item
-        fields = ('id', 'stock_number', 'real_price', 'game_currency_price', 'item_picture')
+        fields = "__all__"
 
-    def get_image(self, obj):
-        if obj.item_picture:
-            with open(os.path.join(settings.MEDIA_ROOT, obj.item_picture.path), 'rb') as image_file:
-                return base64.b64encode(image_file.read()).decode('utf-8')
-        else:
-            return None
