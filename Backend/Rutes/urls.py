c025569faa3c8cd14c46136d9672e1ca907adf83
andreu.sabater <andreu.sabater@estudiantat.upc.edu>
2024-04-13 02:01:52 +0200

    Usuaris

--- Backend/Rutes/urls.py
+++ Backend/Rutes/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from Rutes import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('rutes/', views.rutesApi, name='rutesApi'),
     #path('punts/', views.puntsApi, name='puntsApi'),
     path('puntsInterRuta/', views.AfegirPuntRuta, name='puntsIntermedisApi'),
