8ef2f08da249e95523ecfb7fc5d9493d5803fd0f
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-04-20 23:20:37 +0200

    settings.py

--- apiCrud/wsgi.py
+++ apiCrud/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apiCrud.settings')
 
+print("Variable DJANGO_RUNNING_ON_VM:", os.getenv('DJANGO_RUNNING_ON_VM'))
+
 application = get_wsgi_application()
