6648cfa0748a07322054d6a6ab08b5daffa7fc6b
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-04-20 18:15:52 +0200

    ci/cd

--- Rutes/urls.py
+++ Rutes/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('rutes/', views.rutesApi, name='rutesApi'),
     #path('punts/', views.puntsApi, name='puntsApi'),
     path('puntsInterRuta/', views.AfegirPuntRuta, name='puntsIntermedisApi'),
-    path('puntos-intermedios/<int:rute_id>/', views.puntos_intermedios_list, name='puntos_intermedios'),
+    path('puntos-intermedios/<int:rute_id>/', views.punts_intermedis_list, name='puntos_intermedios'),
 ]
