abb5d1d038c8f8e16b901e77deab4576cc449e3b
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-04-20 11:44:07 +0200

    ci/cd

--- apiCrud/settings.py
+++ apiCrud/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,15 +81,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'apiCrud.wsgi.application'
 
-DATABASES = {
+if 'test' in sys.argv:
+    DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'bikejoy',
-            'USER': 'postgres',
-            'HOST': '172.16.4.38',
-            'PORT': '8080',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'bikejoy',
+                'USER': 'postgres',
+                'HOST': '172.16.4.38',
+                'PORT': '8080',
+            }
+        }
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
