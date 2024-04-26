96c1a6dc5a70fbb8c33a889dfd10ef340443d95a
AnetaMaximaVictoria <edgar.bosque@estudiantat.upc.edu>
2024-04-26 23:35:23 +0200

    Intent serialitzar pets

--- Pets/urls.py
+++ Pets/urls.py
@@ -5,7 +5,7 @@ from Pets import views
 urlpatterns = [
     path('pets/getMascota/<str:name>/', views.get_mascota, name='get_mascota'),
     path('pets/getMascotas/', views.get_mascotas, name='get_mascotas'),
-    path('pets/getMascotasAconseguidesUsuari/<str:nicknameUsuari>/', views.get_mascotas_aconseguides_usuari, name='get_mascotas_aconseguides_usuari'),
+    path('pets/getMascotasAconseguidesUsuari/', views.get_mascotas_aconseguides_usuari, name='get_mascotas_aconseguides_usuari'),
     path('pets/equiparMascota/<str:name>/', views.equipar_mascota, name='equipar_mascota'),
     path('pets/createMascotaAconseguida/<str:name>/', views.create_mascota_aconseguida, name='get_mascota'),
 ]
