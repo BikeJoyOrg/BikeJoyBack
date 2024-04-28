45cb5544bdaab4cb9df6920f4f39c248f22668f8
AnetaMaximaVictoria <edgar.bosque@estudiantat.upc.edu>
2024-04-28 19:21:30 +0200

    Pets amb users

--- Pets/admin.py
+++ Pets/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Mascota
+from .models import Mascota, MascotaAconseguida
 
 admin.site.register(Mascota)
+admin.site.register(MascotaAconseguida)
