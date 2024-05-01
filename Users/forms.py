4d064c4c7e7b8f347f6214c5d2d97d3985c7bb6e
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-05-01 12:21:21 +0200

    fix users

--- Users/forms.py
+++ Users/forms.py
@@ -15,3 +15,11 @@ class CustomUserCreationForm(UserCreationForm):
         if CustomUser.objects.filter(email=email).exists():
             raise forms.ValidationError("Este correo electrónico ya está registrado.")
         return email
+
+    def save(self, commit=True):
+        # Obtener el último ID en la tabla de CustomUser
+        last_id = CustomUser.objects.last().id if CustomUser.objects.exists() else 0
+        # Asignar el siguiente ID disponible
+        self.instance.id = last_id + 1
+        # Guardar el usuario
+        return super().save(commit)
