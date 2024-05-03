4edaa0125879bec25ed585bb91aa588a1921c3b3
AnetaMaximaVictoria <edgar.bosque@estudiantat.upc.edu>
2024-05-03 16:21:18 +0200

    get users

--- Users/models.py
+++ Users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class CustomUser(AbstractUser):
     # Agrega tus campos adicionales aquí. Por ejemplo:
     #campo_extra1 = models.CharField(max_length=100, default='valor predeterminado')
@@ -12,14 +11,3 @@ class CustomUser(AbstractUser):
     weeklyDistance = models.IntegerField(default=0)
     dailyDistance = models.IntegerField(default=0)
 
-
-def reset_monthly_distance():
-    CustomUser.objects.update(monthlyDistance=0)
-
-
-def reset_weekly_distance():
-    CustomUser.objects.update(weeklyDistance=0)
-
-
-def reset_daily_distance():
-    CustomUser.objects.update(dailyDistance=0)
